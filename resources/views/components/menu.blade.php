ed4f7ab4124eb824d6242b96e187c9b3c8fe2b12
Rahmat Sulistio <rr7465457@gmail.com>
2020-08-26 08:41:03 +0700

    Update route and icon component menu

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -1,7 +1,7 @@
 <nav class="nav flex-column">
   @foreach ($listMenu as $menu)
-  <a href="#" class="nav-link {{ $isActive($menu['label']) ? 'active' : '' }}">
-    {{ $menu['label'] }}
+  <a href="{{ route($menu['route']) }}" class="nav-link {{ $isActive($menu['label']) ? 'active' : '' }}">
+    <i class="{{ $menu['icon'] }} mr-2"></i>{{ $menu['label'] }}
   </a>
   @endforeach
 </nav>
--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,10 @@ Auth::routes();
 
 Route::group(['middleware' => ['auth']], function () {
   // Dashboard
-  Route::get('/dashboard', 'Dashboard\DashboardController@index');
+  Route::get('/dashboard', 'Dashboard\DashboardController@index')->name('dashboard');
+  Route::get('/dashboard/movies', 'Dashboard\MovieController@index')->name('dashboard.movies');
+  Route::get('/dashboard/theaters', 'Dashboard\TheaterController@index')->name('dashboard.theaters');
+  Route::get('/dashboard/tickets', 'Dashboard\TicketController@index')->name('dashboard.tickets');
 
   // Users
   Route::get('/dashboard/users', 'Dashboard\UserController@index')->name('dashboard.users');
